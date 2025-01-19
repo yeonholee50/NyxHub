2a0a31d54950a4b4743bfb0c810c4460f12f3727
Yeon Lee <yeonholee50@gmail.com>
2025-01-19 14:25:35 -0500

    fixed api cleanup erros on get post modern

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ async def send_file(
         "file_id": str(file_id),
         "filename": file.filename,
         "sender_id": sender_id,
-        "recipient_id": str(recipient["username"])
+        "recipient_id": recipient_username
     }
     await files_collection.insert_one(file_data)
 
